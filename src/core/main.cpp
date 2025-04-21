2cd0a1281d18cafb360b3f471da699ff5c80f147
Ishan1231 <ishanmishra1231@gmail.com>
2025-04-21 16:29:13 -0400

    created and imported vertex and fragment shaders

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,8 +2,40 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+#include <stdio.h>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+
+// Uses File IO to read and copy a file into a string(const char*)
+const char* LoadShaderSource(const char* filePath) {
+  FILE* file = fopen(filePath, "rb");
+  if (!file) {
+    fprintf(stderr, "Failed to open shader file: %s\n", filePath);
+    return NULL;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long length = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char* buffer = (char*)malloc(length + 1);
+  if (!buffer) {
+      fprintf(stderr, "Failed to allocate memory for shader file.\n");
+      fclose(file);
+      return NULL;
+  }
+
+  fread(buffer, 1, length, file);
+  buffer[length] = '\0';
+  fclose(file);
+  return buffer;
+}
+
+const char *vertexShaderSource = LoadShaderSource("assets/shaders/vertexShader.glsl"); // Vertex Shader source code
+const char *fragmentShaderSource = LoadShaderSource("assets/shaders/fragmentShader.glsl"); // Fragement Shader source code
 
 // Adjusts viewport on incase of resize
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
@@ -11,6 +43,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+// Checks for input
 void processInput(GLFWwindow* window)
 {
     if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS) // Closes window on escape key press
@@ -19,8 +52,7 @@ void processInput(GLFWwindow* window)
 
 int main()
 {
-
-    // Initializing GLFW
+    // initializing GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Major version 3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Minor version 3
@@ -46,11 +78,62 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+   
+    // Vertices of a triangle
+    float vertices[] = {
+      -0.5f, -0.5f, 0.0f, // Bottom left corner
+       0.5f, -0.5f, 0.0f, // Bottom right corner
+       0.5f,  0.5f, 0.0f  // Top 
+    };
+
+    // VBOs are objects which can store large amounts of vertex data in the GPU
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); // Generate buffers. Can do as many as you want as long as they have different types 
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the vertex buffer object to the GL_ARRAY_BUFFER buffer
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Storing user defined vertex data into the curruntly bound buffer
+    
+    // Vertex Shader code 
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER); // Create shader object and assign the type of shader
+    
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // Checks if shader was compiled
+    
+    if(!success)
+    {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    } else {
+      std::cout << "SEEYUH" << std::endl;
+    }
+ 
+    // Fragment Shader code
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vertexShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // Checks if shader was compiled
+
+    if(!success)
+    {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    } else {
+      std::cout << "SEEYUH" << std::endl;
+    }
 
     glViewport(0, 0, WIDTH, HEIGHT); // Sets the bottom left corner of the window.
     // Example of a call back function. We can pass in our own custom functions and register them into these functions to perform specific actions
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+ 
     // Main game loop
     while (!glfwWindowShouldClose(window))
     {
